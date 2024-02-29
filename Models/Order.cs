2aa4c652edacf038befa17e95dd3cbffbe49d959
OmarHamdyx <zoltarscorpio@gmail.com>
2024-02-29 20:52:00 +0200

    Add project files.

---
+++ Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_Simple_E_Commerce.Models
+{
+    public class Order : IValidatableObject
+    {
+        [BindNever]
+        public int OrderNo { get; set; }
+
+        [Range(typeof(DateTime), "2000-01-01", "9999-12-31", ErrorMessage = "The date must be greater than or equal to January 1, 2000.")]
+        public DateTime? OrderDate { get; set; }
+
+        [Required(ErrorMessage = "The {0} is required.")]
+        public double InvoicePrice { get; set; }
+
+        [Required(ErrorMessage = "Please add products")]
+        public List<Product>? Products { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+
+            if (OrderDate == null)
+            {
+                yield return new ValidationResult("OrderDate can't be blank", new string[] { nameof(OrderDate) });
+            }
+
+        }
+
+    }
+
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_Simple_E_Commerce.Models
+{
+    public class Product : IValidatableObject
+    {
+        [Display(Name = "Product Code")]
+        [Required(ErrorMessage = "The {0} is required.")]
+
+        public int ProductCode { get; set; }
+
+        [Required(ErrorMessage = "The {0} is required.")]
+
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "The {0} is required.")]
+
+        public int Quantity { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            
+            if (Price <= 0)
+            {
+                yield return new ValidationResult("Product Price must be between a valid number", new string[] { nameof(Price) });
+            }
+            if (Quantity <= 0)
+            {
+                yield return new ValidationResult("Product Quantity must be between a valid number", new string[] { nameof(Quantity) });
+            }
+        }
+    }
+}
